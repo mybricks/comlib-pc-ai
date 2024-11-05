c95786527e24d89d7281e0c63d5290905a79e1b9
liubiao03 <liubiao03@kuaishou.com>
2024-11-05 21:55:46 +0800

    feat:
    update to one chart

--- src/echarts/editor.tsx
+++ src/echarts/editor.tsx
@@ -1,3 +1,20 @@
-import { getAIEditor } from './common'
+import { getAIEditor, getPromteForAll } from './common'
 
-export default getAIEditor({ systemPrompts: require('./promote.md').default })
+const systemPrompts = `
+${require('./line/promote.md').default}
+${require('./column/promote.md').default}
+${require('./pie/promote.md').default}
+${require('./radar/promote.md').default}
+${require('./funnel/promote.md').default}
+${require('./scatter/promote.md').default}
+${require('./sunburst/promote.md').default}
+${require('./heatmap/promote.md').default}
+${require('./gauge/promote.md').default}
+${require('./tree/promote.md').default}
+${require('./treemap/promote.md').default}
+${require('./sankey/promote.md').default}
+${require('./boxplot/promote.md').default}
+${require('./candlestick/promote.md').default}
+`
+
+export default getAIEditor({ systemPrompts: getPromteForAll({ promte: systemPrompts }) })
