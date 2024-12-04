1d73bc13c302667295aae6473f8af43ddee0bbc0
liubiao03 <liubiao03@kuaishou.com>
2024-12-04 10:27:43 +0800

    feat: col row

--- src/mix/knowledges/antd/Grid.ts
+++ src/mix/knowledges/antd/Grid.ts
@@ -1,13 +1,33 @@
 export default {
   description: `网格布局`,
   editors: {
-    // '.ant-dropdown-trigger': {
-    //   title: '下拉菜单',
+    ':root': {
+      title: '行列',
+      items: [
+        {
+          title: '样式',
+          type: 'style',
+          options: ['background', 'border'],
+        }
+      ]
+    },
+    // '.ant-col': {
+    //   title: '列',
     //   items: [
     //     {
     //       title: '样式',
     //       type: 'style',
-    //       options: ['font', 'background', 'border'],
+    //       options: ['background', 'border'],
+    //     }
+    //   ]
+    // },
+    // '.ant-row': {
+    //   title: '行',
+    //   items: [
+    //     {
+    //       title: '样式',
+    //       type: 'style',
+    //       options: ['background', 'border'],
     //     }
     //   ]
     // },
